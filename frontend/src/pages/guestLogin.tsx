1dd4941735a39bc1822c8b895e125bd0c73d560f
Sahil-4 <www.sahil.875020@gmail.com>
2025-02-09 16:27:46 +0530

    add: created auth pages

--- frontend/src/pages/guestLogin.tsx
+++ frontend/src/pages/guestLogin.tsx
@@ -1,6 +1,37 @@
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+import Header from "../components/header.jsx";
+import styles from "../styles/authForm.module.css";
+
 const GuestLogin = () => {
+  const usernameInput = useRef<HTMLInputElement | null>(null);
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const username = usernameInput.current?.value;
+
+    if (!username) return;
+
+    //
+  };
+
   return (
-    <div>GuestLogin</div>
-  )
-}
-export default GuestLogin
+    <>
+      <Header />
+      <section>
+        <form onSubmit={handleLogin} className={styles.authForm}>
+          <input type="text" placeholder="Username" ref={usernameInput} required={true} />
+          <button type="submit">Login as Guest</button>
+          <div>
+            <Link to={"/login"}>Login</Link>
+            &nbsp;|&nbsp;
+            <Link to={"/signup"}>Sign up</Link>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default GuestLogin;
--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,6 +1,40 @@
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+import Header from "../components/header.jsx";
+import styles from "../styles/authForm.module.css";
+
 const Login = () => {
+  const usernameInput = useRef<HTMLInputElement | null>(null);
+  const passwordInput = useRef<HTMLInputElement | null>(null);
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const username = usernameInput.current?.value;
+    const password = passwordInput.current?.value;
+
+    if (!username || !password) return;
+
+    //
+  };
+
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <>
+      <Header />
+      <section>
+        <form onSubmit={handleLogin} className={styles.authForm}>
+          <input type="text" placeholder="Username" ref={usernameInput} required={true} />
+          <input type="text" placeholder="Password" ref={passwordInput} required={true} />
+          <button type="submit">Login</button>
+          <div>
+            <Link to={"/signup"}>Sign up</Link>
+            &nbsp;|&nbsp;
+            <Link to={"/guestLogin"}>Guest Login</Link>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Login;
