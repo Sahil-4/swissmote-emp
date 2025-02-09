1799278145fa268276f8b10def16a8ba55f32680
Sahil-4 <www.sahil.875020@gmail.com>
2025-02-09 17:26:13 +0530

    add: post event page

--- frontend/src/pages/addEvent.tsx
+++ frontend/src/pages/addEvent.tsx
@@ -1,6 +1,42 @@
+import { useRef } from "react";
+import Header from "../components/header.jsx";
+import styles from "../styles/addEventForm.module.css";
+
 const AddEvent = () => {
+  const addEventFormRef = useRef<HTMLFormElement | null>(null);
+
+  const handleAddEvent = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+    const event: { [key: string]: string } = {};
+    for (let i = 0; i < form.length - 1; i++) {
+      const input = form[i] as HTMLInputElement | HTMLTextAreaElement;
+      event[input.name] = input.value;
+    }
+
+    console.log(event);
+  };
+
   return (
-    <div>AddEvent</div>
-  )
-}
-export default AddEvent
+    <>
+      <Header />
+
+      <section>
+        <form onSubmit={handleAddEvent} ref={addEventFormRef} className={styles.addEventForm}>
+          <h1>Events Details</h1>
+
+          <input name="name" type="text" placeholder="Event Name" required={true} />
+          <textarea name="description" placeholder="Event Description" required={true} />
+          <input name="category" type="text" placeholder="Category" required={true} />
+          <input name="time" type="datetime-local" placeholder="" required={true} />
+          <textarea name="note" placeholder="Notes" />
+
+          <button type="submit">Post</button>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default AddEvent;
